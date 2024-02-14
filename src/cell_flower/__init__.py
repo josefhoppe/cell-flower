96ce6b41187ff79f819d87bd2520beab0b33970f
Josef Hoppe <hoppe@cs.rwth-aachen.de>
2024-02-14 17:00:14 +0100

    Bump version to 0.1.3

--- src/cell_flower/__init__.py
+++ src/cell_flower/__init__.py
@@ -1,7 +1,7 @@
 """
 Cell FLOWer. Implements algorithms and requirements from [1].
 
-[1]: Josef Hoppe and Michael T. Schaub: Representing Edge Flows on Graphs via Sparse Cell Complexes. In: arXiv pre-prints, 2023. https://arxiv.org/abs/2309.01632
+[1]: Josef Hoppe and Michael T. Schaub: Representing Edge Flows on Graphs via Sparse Cell Complexes. In: The Second Learning on Graphs Conference, 2023. https://openreview.net/pdf?id=qix189lq5D
 """
 
 from .cell_complex import CellComplex, normalize_cell, nx_graph_to_cc, cc_to_nx_graph, map_cells_to_index, cell_to_index, index_to_cell
